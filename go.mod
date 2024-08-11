92c1e78a8bf4d1b6a9215dae2bfa915817d35f7b
adarsh010c <adarsh010c@gmail.com>
2024-08-11 17:59:42 +0530

    create signin method logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aadarsh010c/basic-jwt-auth
 
 go 1.22.5
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- signin.go
+++ signin.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
 // create a scecret key , to be used as a signature
 var jwtKey = []byte("my_secret_key")
 
@@ -20,4 +28,48 @@ type Credentials struct {
 type Claims struct {
 	Username string `json:"username"`
 	jwt.RegisteredClaims
+}
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	expectedPassword, ok := users[creds.Username]
+
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	//Claims
+	expirationTime := time.Now().Add(5 * time.Minute)
+
+	claims := &Claims{
+		Username: creds.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	//Declare the token with algorithm used for signing . and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
 }
